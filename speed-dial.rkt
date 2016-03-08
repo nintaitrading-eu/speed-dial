a19b73b653105a3b6a29be2f1ec7c553c5e19d20
Andy Nagels <thereisanewway@gmail.com>
2016-03-08 13:40:42 +0100

    Made the code more clear and compact.

--- speed-dial.rkt
+++ speed-dial.rkt
@@ -3,21 +3,20 @@
 ;(require "menu-main.rkt")
 (require 2htdp/batch-io) ;read-lines
 
+;; constants
+(define C-SPEED-DIAL-CONF "speed-dial.conf")
+
+;; code
 (define (filter-comment-lines a-lines)
   (filter (lambda (a-line)
     (not (equal? (substring a-line 0 1) "#")))
      a-lines))
-
-(define speed-dial-conf
-  (filter-comment-lines (read-lines "speed-dial.conf")))
   
 (define (load-menus-from-file a-file)
-   (printf "Load file here~%")
-   (writeln speed-dial-conf)
-   )
+   (filter-comment-lines (read-lines a-file)))
 
 ;; Main
-(load-menus-from-file "speed-dial.conf")
+(load-menus-from-file C-SPEED-DIAL-CONF)
 ;(show-menu-main)
 
 
