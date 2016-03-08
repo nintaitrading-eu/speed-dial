0124f5fd750cd4974c6fe8e3f1a1566cb70507fd
Andy Nagels <thereisanewway@gmail.com>
2016-03-08 13:28:47 +0100

    Started implementing menu loading from conf file.

--- speed-dial.rkt
+++ speed-dial.rkt
@@ -1,6 +1,23 @@
 #lang racket
 
-(require "menu-main.rkt")
+;(require "menu-main.rkt")
+(require 2htdp/batch-io) ;read-lines
+
+(define (filter-comment-lines a-lines)
+  (filter (lambda (a-line)
+    (not (equal? (substring a-line 0 1) "#")))
+     a-lines))
+
+(define speed-dial-conf
+  (filter-comment-lines (read-lines "speed-dial.conf")))
+  
+(define (load-menus-from-file a-file)
+   (printf "Load file here~%")
+   (writeln speed-dial-conf)
+   )
 
 ;; Main
-(show-menu-main)
+(load-menus-from-file "speed-dial.conf")
+;(show-menu-main)
+
+
