e84631fb9fc8d9b2406a602254c55d0262d8e55c
Andy Nagels <thereisanewway@gmail.com>
2016-03-24 10:24:49 +0100

    Working on the map/filter combination.

--- speed-dial.rkt
+++ speed-dial.rkt
@@ -63,18 +63,22 @@
 ; TODO: finish this
 ; TODO: don't use Menu issues as a category, but use an id.
 (define (filter-list-of-menus a-category-id)
-  ; TODO: make function that returns a shortened list, like described above
   (filter (lambda (a-list)) a-category-id))
+
+; Map a filter to a list. Add this above?
+;(define (category-id-is-equal-to a-list a-category-id)
+;    (map (lambda (a-list)
+;      (list-ref a-list 0)) a-list) a-category-id))
 
 ;; Main
 (define list-of-menus (
   load-menus-from-file C-SPEED-DIAL-CONF))
-(writeln 'debug >>> list-of-menus:)
+(writeln "debug >>> list-of-menus:")
 list-of-menus
-(writeln 'debug >>> Option of second entry)
+(writeln "debug >>> Option of second entry")
 (list-ref (list-ref list-of-menus 1) 1)
-(writeln 'debug >>> filtered issues menu)
-(filter
+(writeln "debug >>> filtered issues menu")
+(filter-list-of-menus list-of-menus)
 ; TODO: filter list-of-menus on a certain ID (add ID to file?)
 ; TODO: map write function to list-of-menus
 ; TODO: sort list-of-menus first?
