5ee3aec29dd82d7580e5c33a8999e11b7b5624dd
Andy Nagels <thereisanewway@gmail.com>
2016-05-11 16:29:21 +0200

    Dynamic retrieval op menu-options implemented.

--- speed-dial.rkt
+++ speed-dial.rkt
@@ -26,19 +26,28 @@
 ; (("0" "1" "Menu issues" "option 1" ...) ("0" "1" "Menu issues" "option 2" ...) ("0" "2" "Menu blabla" "option A" ...))
 ; gives, for filter on 1":
 ; (("0" "1" "Menu issues" "option 1" ...) ("0" "1" "Menu issues" "option 2" ...))
-(define (filter-menu-items a-menu-items a-parent-id)
-  (filter (lambda (x) (equal? (string->number (list-ref x 0)) a-parent-id)) a-menu-items))
+(define (filter-menu-items a-menu-items a-parent-menu-id)
+  (filter (lambda (x) (equal? (string->number (list-ref x 0)) a-parent-menu-id)) a-menu-items))
 
 ; retrieve-menu-items:
-; Retrieves the menu-items with a given a-parent-id,
+; Retrieves the menu-items with a given a-parent-menu-id,
 ; so we know what items we have. Only
-; the unique values are returned, sorted by name.
-; TODO: find out how to replace car with list-ref 1
-(define (retrieve-menu-items a-menu-items a-parent-id)
-;  (filter-menu-items a-menu-items a-parent-id))
+; the unique values are returned, sorted by menu-id.
+(define (retrieve-menu-items a-menu-items a-parent-menu-id)
   (sort
-    (remove-duplicates (filter-menu-items a-menu-items a-parent-id))
+    (remove-duplicates (filter-menu-items a-menu-items a-parent-menu-id))
     #:key (lambda (x) (car (cdr x))) string<?))
+
+; retrieve-menu-options:
+; Retrieves the menu-options for a given
+; parent-menu-id.
+; Example:
+; '(("1" "1" "a"...) ("1" "2" "b"...))
+; will give '("a" "b") as a result.
+; TODO: finish this.
+(define (retrieve-menu-options a-menu-items a-parent-menu-id)
+  (map (lambda (x) (string->symbol x))
+    (map (lambda (x) (list-ref x 3)) (retrieve-menu-items a-menu-items a-parent-menu-id))))
 
 ; write-main-menu:
 ; Write the main menu items, based on a given list
@@ -55,24 +64,21 @@
 ; list a-menus.
 ; Note: Used for displaying the main menu.
 ; This also starts the option parsing loop.
-; TODO: write a retrieve-menu-options functions, similar
-; to retrieve-menu-items
-(define (show-menu a-menu-items a-parent-id)
+(define (show-menu a-menu-items a-parent-menu-id)
     (system "clear")
     (print-header "Menu")
-    (print-menu (retrieve-menu-items a-menu-items a-parent-id))
-    (loop-choice a-menu-items a-parent-id (list 'a 'i 'o))
-    )
+    (print-menu (retrieve-menu-items a-menu-items a-parent-menu-id))
+    (loop-choice a-menu-items a-parent-menu-id (retrieve-menu-options a-menu-items a-parent-menu-id)))
 
 ; loop-choice:
 ; Loop that displays the menu, until an option is chosen.
 ; TODO: make the choice condition dependend on a-choice-list.
-(define (loop-choice a-menu-items a-parent-id a-choice-list)
+(define (loop-choice a-menu-items a-parent-menu-id a-choice-list)
     (let ((l-choice (read)))
     (cond ((member l-choice a-choice-list) (run-choice l-choice a-menu-items))
           ((eq? l-choice 'q) (run-quit))
           (else (print-menu-error)))
-    (show-menu a-menu-items a-parent-id)))
+    (show-menu a-menu-items a-parent-menu-id)))
 
 ; run-choice:
 ; Execute the correct action, belonging to the chosen option
@@ -91,9 +97,4 @@
 ; Main
 ;-------------------------------------------
 
-;(writeln "debug >>> filtered issues menu")
-;(filter-list-of-menus list-of-menus "1")
-;(writeln "debug >>> categories")
-;(retrieve-menu-categories list-of-menus)
 (show-menu list-of-menus 0) ; Show main menu
-;(show-menu-main)
